77e23293e0988832d9e314f9cc7bf03a85fc3ea5
Matt <matt@solarhorizon.dev>
2026-03-12 15:23:23 -0400

    fix mc console script

--- modules/services/minecraft/minecraft.nix
+++ modules/services/minecraft/minecraft.nix
@@ -11,7 +11,7 @@
 
     environment.systemPackages = with pkgs; [
       (writeShellScriptBin "mc-console" ''
-        su minecraft -s $SHELL -c "tmux -S /run/minecraft/${1}.sock attach"
+        su minecraft -s $SHELL -c "tmux -S /run/minecraft/''${1}.sock attach"
       '')
       tmux
     ];

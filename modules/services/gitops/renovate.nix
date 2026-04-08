2bb51c78a867a42c6e5272b44924869cc84b8fb7
Matt <matt@solarhorizon.dev>
2026-04-07 21:33:45 -0400

    renovate bot

---
+++ modules/services/gitops/renovate.nix
@@ -0,0 +1,33 @@
+{self, ...}: {
+  flake.modules.nixos.renovate = {
+    config,
+    pkgs,
+    ...
+  }: {
+    sops.secrets."renovate/forgejo-token" = {
+      sopsFile = ../../../secrets/services/renovate.yaml;
+    };
+
+    services.renovate = {
+      enable = true;
+      schedule = "*:0/30";
+      credentials = {
+        RENOVATE_TOKEN = config.sops.secrets."renovate/forgejo-token".path;
+      };
+      runtimePackages = [pkgs.nix];
+      settings = {
+        platform = "forgejo";
+        endpoint = "https://git.matt.you/api/v1";
+        gitAuthor = "Renovate Bot <renovate@git.matt.you>";
+        autodiscover = true;
+        autodiscoverTopics = ["renovate"];
+      };
+    };
+  };
+
+  flake.modules.nixos.monolith = {
+    imports = with self.modules.nixos; [
+      renovate
+    ];
+  };
+}
